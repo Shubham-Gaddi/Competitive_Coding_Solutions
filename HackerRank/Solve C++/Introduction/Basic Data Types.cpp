dcd8e4ba5c4152165e3db86e6ae6f11d15de429c
Shubham Gaddi <shubhamgdd@gmail.com>
2020-06-15 16:19:11 +0530

    Update Basic Data Types.cpp

--- HackerRank/Solve C++/Introduction/Basic Data Types.cpp
+++ HackerRank/Solve C++/Introduction/Basic Data Types.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main() {
     // Complete the code.
+    int n;
+    long l;
+    char c;
+    float f;
+    double d;
+    scanf("%d%ld%*c%c%f%lf", &n, &l, &c, &f, &d);
+    printf("%d\n%ld\n%c\n%.3f\n%.9lf\n", n , l, c, f, d);
     return 0;
 }
